08f027426a5573f834f0390369545d678c1f6e20
Vongola <1085280247@qq.com>
2018-08-18 16:27:06 +0800

    完成启动app

---
+++ WWW/app.py
@@ -0,0 +1,12 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+from aiohttp import web
+def index(request):
+    return web.Response(body='<h1>AWesome</h1>'.encode('utf-8'),content_type='text/html')
+
+def init():
+    app = web.Application()
+    app.router.add_route('GET', '/', index)
+    web.run_app(app, host='127.0.0.1', port=9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+init()
